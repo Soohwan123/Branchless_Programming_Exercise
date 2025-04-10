afc58e0a52d9722c00ab935bb087465badad65af
Soohwan <rlatnghks789456@gmail.com>
2025-04-09 21:16:23 -0400

    Update for max

--- branch_max.c
+++ branch_max.c
@@ -1,10 +1,12 @@
 #include "stdlib.h"
 #include "stdio.h"
 
+//브랜치 함수 -> 성능 느림
 int branched_max(int a, int b) {
     return a > b ? a : b;
 }
 
+//브랜치리스 함수 -> 성능 빠름
 int unbranched_max(int a, int b) {
     return a + ((b - a) & ((b - a) >> 31));
 }
